dd9afe26dc7119a59286bf813666b1353f1ad298
Tristan Bester <tristanbester@gmail.com>
2022-08-09 23:26:56 +0200

    Add implementation to merge clusters

--- src/lib.rs
+++ src/lib.rs
@@ -54,8 +54,75 @@ fn form_clusters<'a>(inputs: &Vec<&'a str>, tol: f32) -> Vec<Vec<&'a str>> {
     container
 }
 
+fn merge_clusters<'a>(set_one: &mut Vec<Vec<&'a str>>, set_two: &mut Vec<Vec<&'a str>>, tol: f32) {
+    let mut moved = vec![false; set_two.len()];
+
+    for i in 0..set_one.len() {
+        for j in 0..set_two.len() {
+            if moved[j] {
+                continue;
+            }
+
+            if is_similar(set_one[i][0], set_two[j][0], tol) {
+                set_one[i].append(&mut set_two[j]);
+                moved[j] = true;
+            }
+        }
+    }
+
+    for (i, m) in moved.iter().enumerate() {
+        if !*m {
+            set_one.push(set_two[i].clone());
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    mod merge_clusters {
+        use crate::merge_clusters;
+
+        #[test]
+        fn test_merge_no_overlap() {
+            let mut set_one = vec![vec!["a"], vec!["b"]];
+            let mut set_two = vec![vec!["c"], vec!["d"]];
+            let expected = vec![vec!["a"], vec!["b"], vec!["c"], vec!["d"]];
+
+            merge_clusters(&mut set_one, &mut set_two, 0.0);
+            assert_eq!(set_one, expected);
+        }
+
+        #[test]
+        fn test_merge_full_overlap() {
+            let mut set_one = vec![vec!["aa"], vec!["bb"]];
+            let mut set_two = vec![vec!["aa"], vec!["bb"]];
+            let expected = vec![vec!["aa", "aa"], vec!["bb", "bb"]];
+
+            merge_clusters(&mut set_one, &mut set_two, 0.5);
+            assert_eq!(set_one, expected);
+        }
+
+        #[test]
+        fn test_partial_overlap() {
+            let mut set_one = vec![vec!["aa"], vec!["bb"]];
+            let mut set_two = vec![vec!["aa"], vec!["bb"], vec!["cc"]];
+            let expected = vec![vec!["aa", "aa"], vec!["bb", "bb"], vec!["cc"]];
+
+            merge_clusters(&mut set_one, &mut set_two, 0.5);
+            assert_eq!(set_one, expected);
+        }
+
+        #[test]
+        fn test_merge_tol_correct() {
+            let mut set_one = vec![vec!["aa"], vec!["cc"]];
+            let mut set_two = vec![vec!["ab"], vec!["cd"]];
+            let expected = vec![vec!["aa", "ab"], vec!["cc", "cd"]];
+
+            merge_clusters(&mut set_one, &mut set_two, 0.5);
+            assert_eq!(set_one, expected);
+        }
+    }
+
     mod init_container {
         use crate::init_container;
 

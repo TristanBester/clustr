7db3b03de89423ac60755d92bfde84e4e5b6e29b
Tristan Bester <tristanbester@gmail.com>
2022-08-09 22:42:20 +0200

    Added clustering implementation, test & refactored

--- src/lib.rs
+++ src/lib.rs
@@ -1,106 +1,125 @@
-use edit_distance::edit_distance;
+mod utils;
+use crate::utils::is_similar;
 
-fn get_max_edit_dist(a: &String, b: &String, tol: f32) -> usize {
-    let max_edit: f32;
-    let l_a = a.len() as f32;
-    let l_b = b.len() as f32;
-
-    if l_a < l_b {
-        max_edit = l_a * tol;
-    } else {
-        max_edit = l_b * tol;
+fn init_container<'a>(inputs: &Vec<&'a str>) -> Vec<Vec<&'a str>> {
+    if inputs.len() == 0 {
+        panic!("inputs cannot be empty.");
     }
-    max_edit as usize
+
+    let mut container = vec![Vec::new(); inputs.len()];
+
+    for (i, s) in inputs.iter().enumerate() {
+        container[i].push(*s);
+    }
+    container
 }
 
-fn is_similar(a: &String, b: &String, tol: f32) -> bool {
-    let max_edit = get_max_edit_dist(a, b, tol) as i32;
-    let len_diff = (a.len() as i32 - b.len() as i32).abs();
-
-    // If difference in length between string is greater than max
-    // edit distance it is not possible for the strings to be similar
-    if len_diff > max_edit {
-        return false;
+fn form_clusters<'a>(inputs: &Vec<&'a str>, tol: f32) -> Vec<Vec<&'a str>> {
+    if inputs.len() == 0 {
+        panic!("inputs cannot be empty.");
     }
 
-    let dist = edit_distance(a, b);
-    dist <= max_edit as usize
+    let mut container = init_container(inputs);
+
+    // Store if value has been moved into a cluster
+    let mut moved = vec![false; inputs.len()];
+    // Store if value is cluster representative
+    let mut is_repr = vec![false; inputs.len()];
+
+    for i in 0..inputs.len() {
+        if moved[i] {
+            // A value cannot be moved into multiple clusters
+            continue;
+        } else {
+            // If value has not been moved into any of the previous clusters
+            // it forms a new cluster
+            is_repr[i] = true;
+        }
+
+        for j in i + 1..inputs.len() {
+            if is_similar(container[i][0], container[j][0], tol) {
+                let str_ref = container[j][0];
+                container[i].push(str_ref);
+                moved[j] = true;
+            }
+        }
+    }
+
+    // Delete single value vectors remaining after values moved into cluster
+    for (i, v) in moved.iter().enumerate().rev() {
+        if *v {
+            container.remove(i);
+        }
+    }
+    container
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    mod init_container {
+        use crate::init_container;
 
-    #[test]
-    fn is_similar_accepts_below_max_edit() {
-        let a = "aaaa".to_string();
-        let b = "aaab".to_string();
+        #[test]
+        fn test_structure_correct() {
+            let inputs = vec!["a", "b"];
+            let expected = vec![vec!["a"], vec!["b"]];
 
-        let result = is_similar(&a, &b, 0.5);
-        assert!(result);
+            let results = init_container(&inputs);
+            assert_eq!(results, expected);
+        }
+
+        #[test]
+        #[should_panic(expected = "inputs cannot be empty.")]
+        fn test_reject_empty() {
+            let inputs = Vec::new();
+            init_container(&inputs);
+        }
     }
 
-    #[test]
-    fn is_similar_accepts_max_edit() {
-        let a = "aaaa".to_string();
-        let b = "aabb".to_string();
+    mod form_clusters {
+        use crate::form_clusters;
 
-        let result = is_similar(&a, &b, 0.5);
-        assert!(result);
-    }
+        #[test]
+        fn test_cluster_correct() {
+            let inputs = vec!["a", "a", "b", "b"];
+            let expected = vec![vec!["a", "a"], vec!["b", "b"]];
 
-    #[test]
-    fn is_similar_rejects_above_max_edit() {
-        let a = "a".to_string();
-        let b = "abbb".to_string();
+            let results = form_clusters(&inputs, 0.0);
+            assert_eq!(results, expected);
+        }
 
-        let result = is_similar(&a, &b, 1.0);
-        assert!(!result);
-    }
+        #[test]
+        fn test_clusters_formed_below_tol() {
+            let inputs = vec!["aaa", "aac", "bbb", "bbc"];
+            let expected = vec![vec!["aaa", "aac"], vec!["bbb", "bbc"]];
 
-    #[test]
-    fn max_edit_calculation_correct() {
-        let a = "aa".to_string();
-        let b = "bbb".to_string();
+            let results = form_clusters(&inputs, 0.34);
+            assert_eq!(results, expected);
+        }
 
-        let result = get_max_edit_dist(&a, &b, 0.5);
-        assert_eq!(result, 1);
-    }
+        #[test]
+        fn test_clusters_formed_equal_tol() {
+            let inputs = vec!["aa", "ab", "cc", "cd"];
+            let expected = vec![vec!["aa", "ab"], vec!["cc", "cd"]];
 
-    #[test]
-    fn max_edit_calculation_applies_floor() {
-        let a = "aaaa".to_string();
-        let b = "bbbb".to_string();
+            let results = form_clusters(&inputs, 0.5);
+            assert_eq!(results, expected);
+        }
 
-        let result = get_max_edit_dist(&a, &b, 0.49);
-        assert_eq!(result, 1);
-    }
+        #[test]
+        fn test_no_clusters() {
+            let inputs = vec!["a", "b", "c"];
+            let expected = vec![vec!["a"], vec!["b"], vec!["c"]];
 
-    #[test]
-    fn max_edit_shorted_string_selected() {
-        let a = "aa".to_string();
-        let b = "bbb".to_string();
+            let results = form_clusters(&inputs, 0.0);
+            assert_eq!(results, expected);
+        }
 
-        let op_one = get_max_edit_dist(&a, &b, 0.5);
-        let op_two = get_max_edit_dist(&b, &a, 0.5);
-        assert_eq!(op_one, op_two);
-    }
-
-    #[test]
-    fn max_edit_handles_empty() {
-        let a = "".to_string();
-        let b = "bbb".to_string();
-
-        let result = get_max_edit_dist(&a, &b, 0.5);
-        assert_eq!(result, 0);
+        #[test]
+        #[should_panic(expected = "inputs cannot be empty.")]
+        fn test_reject_empty() {
+            let inputs = Vec::new();
+            form_clusters(&inputs, 0.0);
+        }
     }
 }
-
-// #[cfg(test)]
-// mod tests {
-//     #[test]
-//     fn it_works() {
-//         let result = 2 + 2;
-//         assert_eq!(result, 4);
-//     }
-// }
---
+++ src/utils.rs
@@ -0,0 +1,103 @@
+use edit_distance::edit_distance;
+
+pub fn is_similar(a: &str, b: &str, tol: f32) -> bool {
+    let max_edit = get_max_edit_dist(a, b, tol) as i32;
+    let len_diff = (a.len() as i32 - b.len() as i32).abs();
+
+    // If difference in length between strings is greater than max
+    // edit distance it is not possible for the strings to be similar
+    if len_diff > max_edit {
+        return false;
+    }
+
+    let dist = edit_distance(a, b);
+    dist <= max_edit as usize
+}
+
+fn get_max_edit_dist(a: &str, b: &str, tol: f32) -> usize {
+    let max_edit: f32;
+    let l_a = a.len() as f32;
+    let l_b = b.len() as f32;
+
+    if l_a < l_b {
+        max_edit = l_a * tol;
+    } else {
+        max_edit = l_b * tol;
+    }
+    max_edit as usize
+}
+
+#[cfg(test)]
+mod tests {
+    mod is_similar {
+        use crate::utils::is_similar;
+
+        #[test]
+        fn is_similar_accepts_below_max_edit() {
+            let a = "aaaa";
+            let b = "aaab";
+
+            let result = is_similar(&a, &b, 0.5);
+            assert!(result);
+        }
+
+        #[test]
+        fn is_similar_accepts_max_edit() {
+            let a = "aaaa";
+            let b = "aabb";
+
+            let result = is_similar(&a, &b, 0.5);
+            assert!(result);
+        }
+
+        #[test]
+        fn is_similar_rejects_above_max_edit() {
+            let a = "a";
+            let b = "abbb";
+
+            let result = is_similar(&a, &b, 1.0);
+            assert!(!result);
+        }
+    }
+
+    mod max_edit_distance {
+        use crate::utils::get_max_edit_dist;
+
+        #[test]
+        fn max_edit_calculation_correct() {
+            let a = "aa";
+            let b = "bbb";
+
+            let result = get_max_edit_dist(&a, &b, 0.5);
+            assert_eq!(result, 1);
+        }
+
+        #[test]
+        fn max_edit_calculation_applies_floor() {
+            let a = "aaaa";
+            let b = "bbbb";
+
+            let result = get_max_edit_dist(&a, &b, 0.49);
+            assert_eq!(result, 1);
+        }
+
+        #[test]
+        fn max_edit_shorted_string_selected() {
+            let a = "aa";
+            let b = "bbb";
+
+            let op_one = get_max_edit_dist(&a, &b, 0.5);
+            let op_two = get_max_edit_dist(&b, &a, 0.5);
+            assert_eq!(op_one, op_two);
+        }
+
+        #[test]
+        fn max_edit_handles_empty() {
+            let a = "";
+            let b = "bbb";
+
+            let result = get_max_edit_dist(&a, &b, 0.5);
+            assert_eq!(result, 0);
+        }
+    }
+}
